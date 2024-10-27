2a7c552bf68dc696f5760ec6f92da0042ed170b8
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2024-10-27 19:41:15 +0300

    Delete lab_1 directory

--- lab_1/lab_1/ICalculator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lab_1
-{
-    internal interface ICalculator
-        {
-         string Calc(double mass, double height, uint years, double targetMass, uint timeLine);
-        }
-    }
-
-
