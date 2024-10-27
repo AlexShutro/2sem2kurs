6ac8698f1335b271735f5adad9f3180cdf751c14
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2024-10-27 19:53:06 +0300

    Add files via upload

---
+++ OOP/lab06/WpfShop/UserControls/UserTitle.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfShop.UserControls {
+    /// <summary>
+    /// Логика взаимодействия для UserTitle.xaml
+    /// </summary>
+    public partial class UserTitle : UserControl {
+        public UserTitle() {
+            InitializeComponent();
+        }
+
+        private void Control_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textBlock1.Text = textBlock1.Text + "sender: " + sender.ToString() + "\n";
+            textBlock1.Text = textBlock1.Text + "source: " + e.Source.ToString() + "\n\n";
+        }
+    }
+}
