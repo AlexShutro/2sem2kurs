015bada12995110bb94e22a2e254a1623da75a3f
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2024-10-27 19:40:56 +0300

    Add files via upload

---
+++ lab_1/lab_1/ICalculator.cs
@@ -0,0 +1,9 @@
+﻿namespace lab_1
+{
+    internal interface ICalculator
+        {
+         string Calc(double mass, double height, uint years, double targetMass, uint timeLine);
+        }
+    }
+
+
